e5de12e46cd61a936706256c437b1737111e09dd
baosphoa0403 <baosphoa0403@gmail.com>
2021-06-07 21:56:41 +0700

    Add code

--- Test/login.test.js
+++ Test/login.test.js
@@ -12,43 +12,39 @@ describe('login with user', () => {
     },
     messager: 'login success',
   };
-  try {
-    beforeEach(function () {
-      console.log('login with account user email vs password');
-    });
-    // setup
+  beforeEach(function () {
+    console.log('login with account user email vs password');
+  });
+  // setup
 
-    // testing
-    test('call api login', async () => {
-      const user = {
-        email: 'khoa1234@gmail.com',
-        password: 'khoa12345',
-      };
-      try {
-        const response = await userService.login(user);
-        console.log(response.data);
-        //   expect(response.data).toHaveProperty('user');
-        //   expect(response.data).toHaveProperty('token');
-        //   expect(response.data).toHaveProperty('messager');
-        delete response.data.token;
-        expect(data).toStrictEqual(response.data);
-      } catch (error) {
-        // console.log({...error});
-        // console.log(error.response.status);
-        // console.log(error.response.data);
-        // expect(error.response.status).toBe(400);
-        // console.log(BadRequestError.STATUS_CODE);
-        // expect(error.response.status).toBe(BadRequestError.STATUS_CODE);
-        expect(error.response.data).toEqual(
-          expect.stringContaining('Unable to login')
-        );
-      }
-    });
-    //done
-    afterEach(function () {
-      console.log('Call api login done');
-    });
-  } catch (error) {
-    console.log(error);
-  }
+  // testing
+  test('call api login', async () => {
+    const user = {
+      email: 'khoa1234@gmail.com',
+      password: 'khoa12345',
+    };
+    try {
+      const response = await userService.login(user);
+      console.log(response.data);
+      //   expect(response.data).toHaveProperty('user');
+      //   expect(response.data).toHaveProperty('token');
+      //   expect(response.data).toHaveProperty('messager');
+      delete response.data.token;
+      expect(data).toStrictEqual(response.data);
+    } catch (error) {
+      // console.log({...error});
+      // console.log(error.response.status);
+      // console.log(error.response.data);
+      // expect(error.response.status).toBe(400);
+      // console.log(BadRequestError.STATUS_CODE);
+      // expect(error.response.status).toBe(BadRequestError.STATUS_CODE);
+      expect(error.response.data).toEqual(
+        expect.stringContaining('Unable to login')
+      );
+    }
+  });
+  //done
+  afterEach(function () {
+    console.log('Call api login done');
+  });
 });
